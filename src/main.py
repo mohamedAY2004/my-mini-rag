b23971693d91382678e516555edfa47855645cab
Mohamed <mido.yones1@gmail.com>
2026-02-11 06:05:44 +0200

    used MVC project architicture and used pydantic-settings to handle env file validation and for cleaner code

--- src/main.py
+++ src/main.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
 from routes.base import base_router
-import dotenv
-dotenv.load_dotenv(".env")
 app = FastAPI()
 app.include_router(base_router)
--- src/routes/base.py
+++ src/routes/base.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI, APIRouter
-import os
+from fastapi import FastAPI, APIRouter, Depends
+from helpers.config import get_settings, Settings
 base_router = APIRouter(
     prefix="/api/v1",
 # tags is for the documentation
     tags=["base"]
 )
 @base_router.get("/")
-async def welcome():
-    return {"message": f"Welcome to  {os.getenv('APP_NAME')} version {os.getenv('APP_VERSION')} by {os.getenv('APP_AUTHOR')}"}
+async def welcome(app_settings: Settings = Depends(get_settings)):
+    return {"message": f"Welcome to  {app_settings.APP_NAME} version {app_settings.APP_VERSION} by {app_settings.APP_AUTHOR}"}

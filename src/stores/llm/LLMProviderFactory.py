233e412b6aaac018d7fdd2decb553066cca82eaa
mohamedAY2004 <mido.yones1@gmail.com>
2026-04-16 01:27:59 +0200

    simple bug fix at LLMFactory the defaults aren't sent to gemini and cohere provider

--- src/stores/llm/LLMProviderFactory.py
+++ src/stores/llm/LLMProviderFactory.py
@@ -8,7 +8,11 @@ class LLMProviderFactory:
 
     def create(self,provider: str):
         if provider == LLMEnums.GEMINI.value:
-            return GeminiProvider(api_key=self.config.GEMINI_API_KEY)
+            return GeminiProvider(api_key=self.config.GEMINI_API_KEY,
+            default_input_max_characters=self.config.DEFAULT_INPUT_MAX_CHARACTERS,
+            default_generation_max_tokens=self.config.DEFAULT_GENERATION_MAX_TOKENS,
+            default_temperature=self.config.DEFAULT_GENERATION_TEMPERATURE,
+            )
         elif provider == LLMEnums.OPENAI.value:
             return OpenAIProvider(api_key=self.config.OPENAI_API_KEY,
             api_url=self.config.OPENAI_API_URL,
@@ -17,4 +21,8 @@ class LLMProviderFactory:
             default_temperature=self.config.DEFAULT_GENERATION_TEMPERATURE,
             )
         elif provider == LLMEnums.COHERE.value:
-            return CoHereProvider(api_key=self.config.COHERE_API_KEY)
+            return CoHereProvider(api_key=self.config.COHERE_API_KEY,
+            default_input_max_characters=self.config.DEFAULT_INPUT_MAX_CHARACTERS,
+            default_generation_max_tokens=self.config.DEFAULT_GENERATION_MAX_TOKENS,
+            default_temperature=self.config.DEFAULT_GENERATION_TEMPERATURE,
+            )
